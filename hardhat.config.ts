f2734ed8c7207ed3a639e26c30d19878298fb5cd
Behnam Azimi <bhnmzm@gmail.com>
2024-05-15 21:17:42 +0200

    Add Arbitrum Sepolia config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "hardhat-contract-sizer";
 
 dotenv.config();
 
-let amoy;
+let networks = {};
 
 // If not running in GitHub Actions, add the Amoy network
 if (
@@ -14,15 +14,32 @@ if (
   process.env.POLYGON_AMOY_RPC_URL &&
   process.env.POLYGON_AMOY_PRIVATE_KEY
 ) {
-  amoy = {
-    url: process.env.POLYGON_AMOY_RPC_URL,
-    accounts: [process.env.POLYGON_AMOY_PRIVATE_KEY],
+  networks = {
+    ...networks,
+    amoy: {
+      url: process.env.POLYGON_AMOY_RPC_URL,
+      accounts: [process.env.POLYGON_AMOY_PRIVATE_KEY],
+    },
+  };
+}
+
+if (
+  !process.env.GITHUB_ACTIONS &&
+  process.env.ARBITRUM_SEPOLIA_RPC_URL &&
+  process.env.ARBITRUM_SEPOLIA_PRIVATE_KEY
+) {
+  networks = {
+    ...networks,
+    arbitrumSepolia: {
+      url: process.env.ARBITRUM_SEPOLIA_RPC_URL,
+      accounts: [process.env.ARBITRUM_SEPOLIA_PRIVATE_KEY],
+    },
   };
 }
 
 const config: HardhatUserConfig = {
   defaultNetwork: "localhost",
-  networks: amoy ? { amoy } : {},
+  networks,
   solidity: {
     version: "0.8.24",
     settings: {
