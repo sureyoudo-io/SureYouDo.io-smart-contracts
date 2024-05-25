d7c4060275242e2207c5db71fff6bd8cedb0a8ce
Behnam Azimi <bhnmzm@gmail.com>
2024-05-25 23:41:29 +0200

    Add updateMinPlatformCommission factory

--- scripts/tasks/index.ts
+++ scripts/tasks/index.ts
@@ -2,3 +2,4 @@ export * as deploy from "./deploy";
 export * as addCharity from "./addCharity";
 export * as setSydTokenAddress from "./setSydTokenAddress";
 export * as updateMaxParticipants from "./updateMaxParticipants";
+export * as updateMinPlatformCommission from "./updateMinPlatformCommission";
---
+++ scripts/tasks/updateMinPlatformCommission.ts
@@ -0,0 +1,71 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { SureYouDo__factory } from "../../typechain-types";
+import logger from "../utils/logger";
+import reportFactory, {
+  DeployReport,
+  UpdateMaxParticipantsReport,
+} from "../utils/reports";
+
+const updateMinPlatformCommission = async (
+  {
+    regular,
+    pro,
+  }: {
+    regular: string;
+    pro: string;
+  },
+  hre: HardhatRuntimeEnvironment,
+): Promise<void> => {
+  try {
+    const targetNetwork = hre.network;
+
+    const { ethers } = hre;
+    const reports = reportFactory(targetNetwork.name);
+
+    logger.info(
+      `Updating min platform commission on "${targetNetwork.name}" network`,
+    );
+    const [owner] = await ethers.getSigners();
+
+    // Get the deployed SYD contract address from reports
+    const deployReport = reports.readReport<DeployReport>("deploy");
+    if (!deployReport) {
+      logger.error(`No deployment found for "${targetNetwork.name}" network`);
+      return;
+    }
+    const sydAddress = deployReport.deployedAt.SureYouDo || "";
+
+    // init the SYD contract
+    const syd = SureYouDo__factory.connect(sydAddress, owner);
+
+    // update the min platform commission in the main contract
+    const tnx = await syd.updateMinPlatformCommission(regular, pro);
+    await tnx.wait();
+    logger.info("Min platform commission updated successfully!");
+
+    // Update reports
+    const updatedReport = [
+      ...(reports.readReport<UpdateMaxParticipantsReport[]>(
+        "updateMinPlatformCommission",
+      ) || []),
+      {
+        timestamp: Date.now(),
+        regular,
+        pro,
+      },
+    ];
+
+    reports.writeReport("updateMinPlatformCommission", updatedReport);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+task(
+  "syd:update-min-platform-commission",
+  "Update the min platform commission",
+  updateMinPlatformCommission,
+)
+  .addParam("regular", "The new max participants value for regular accounts")
+  .addParam("pro", "The new max participants value for Pro accounts");
